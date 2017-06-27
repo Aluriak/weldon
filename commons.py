ab225780b9303c4a8930e9a838a1638c962d996a
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2017-06-27 19:34:23 +0200

    SubmissionResult is now a pure class providing new behaviors

--- commons.py
+++ commons.py
@@ -6,5 +6,25 @@ from collections import namedtuple
 
 
 TEST_TYPES = {'hidden', 'public', 'community'}
-SubmissionResult = namedtuple('SubmissionResult', 'tests full_trace problem_id')
 TestResult = namedtuple('TestResult', 'name type succeed')
+
+
+class SubmissionResult():
+    """Object created by the server and returned to the player"""
+
+    def __init__(self, tests, full_trace, problem_id):
+        self._tests = dict(tests)
+        self._full_trace = str(full_trace)
+        self._problem_id = int(problem_id)
+        self._total_success = all(test.succeed for test in tests.values())
+
+    @property
+    def total_success(self): return self._total_success
+    @property
+    def tests(self): return self._tests
+    @property
+    def full_trace(self): return self._full_trace
+    @property
+    def problem_id(self): return self._problem_id
+    @property
+    def total_success(self): return self._total_success
