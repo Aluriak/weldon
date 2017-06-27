006a33d77b25aba13470febf1c27ce10d9ef6f95
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2017-06-27 22:52:11 +0200

    commons: define entry point for custom class JSON serializing

--- commons.py
+++ commons.py
@@ -2,6 +2,7 @@
 
 """
 
+import json
 from collections import namedtuple
 
 
@@ -9,7 +10,36 @@ TEST_TYPES = {'hidden', 'public', 'community'}
 TestResult = namedtuple('TestResult', 'name type succeed')
 
 
-class SubmissionResult():
+def custom_json_encoder(cls:type or [type]) -> json.JSONEncoder:
+    """Return a class ready to be used by json module to encode given
+    class(es) of custom objects.
+
+    Classes must provide a as_json() method returning json serializable data.
+
+    See https://docs.python.org/3.6/library/json.html for more background.
+
+    """
+    class CustomObjectEncoder(json.JSONEncoder):
+        def default(self, obj):
+            if isinstance(obj, cls):
+                return obj.to_json()
+            # Let the base class default method raise the TypeError
+            return json.JSONEncoder.default(self, obj)
+    return CustomObjectEncoder
+
+def custom_json_decoder(classes:[type]):
+    def custom_object_decoder(dct):
+        for cls in classes:
+            interp = cls.from_json(dct)
+            if interp:
+                return interp
+        # nothing found
+        return dct
+    return custom_object_decoder
+
+
+
+class SubmissionResult:
     """Object created by the server and returned to the player"""
 
     def __init__(self, tests, full_trace, problem_id):
