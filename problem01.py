1364336aba34ccd4ca49f3b86eddf1228cc7ef32
Aluriak <lucas.bourneuf@laposte.net>
2017-07-01 01:54:14 +0200

    problem01: make the story testable with pytest
    so it's also a unit test \o/

--- problem01.py
+++ problem01.py
@@ -13,6 +13,115 @@ import pytest
 import server as weldon
 
 
+def test_story_problem01():
+    print('#' * 80)
+    print('# ROOTER PART')
+    print('#' * 80)
+
+    # Registering is performed by the rooter.
+    # It gives to the server the exact definition of the problem,
+    #  that any player can retrieve.
+    PLAYER_PASSWORD = 'WOLOLO42'
+    server = weldon.Server(player_password=PLAYER_PASSWORD)
+    rooter_token = server.register_rooter('gérard')
+    print("Rooter register itself as a rooter, getting (secret) token {}."
+          "".format(rooter_token))
+    problem = server.register_problem(
+        rooter_token,
+        'problem01',
+        DESCRIPTION,
+        public_tests=PUBLIC_TESTS,
+        hidden_tests=HIDDEN_TESTS,
+        # TODO: implement rules (or additionnal options on problem)
+        # rules=['allow upload test if all tests succeed',
+               # 'allow upload solution if player or rooter']  # should be a default
+    )
+    assert problem.title == 'problem01'
+    assert problem.description == DESCRIPTION
+
+    print("Rooter, using (secret) token, create a new problem on the server.")
+    print("Rooter gives to the player the password '{}', to use to register."
+          "".format(PLAYER_PASSWORD))
+
+
+
+
+    print('\n\n')
+    print('#' * 80)
+    print('# PLAYER PART')
+    print('#' * 80)
+
+    print("First, i need to register on server, as a player."
+          " Rooter gives me the password '{}'".format(PLAYER_PASSWORD))
+    my_token = server.register_player('lucas', password=PLAYER_PASSWORD)
+    print('My (secret) token is', my_token)
+
+    print('\nUsing my token, i can ask for specific problem at server.')
+    problem = server.retrieve_public_problem(my_token, 'problem01')
+    print('The problem given by the rooter is {}.'.format(problem.title))
+    print('Full object given by server:\n', problem, '\n\n', sep='')
+
+    print("\nNote that, as a player, i can't submit new problems.")
+    with pytest.raises(ServerError):
+        print(server.register_problem(my_token, '', '', '', ''))
+
+    print('I code my solution, and send it to the server using my token.')
+    server_answer = server.submit_solution(my_token, problem.id, BAD_SOLUTION)
+    print("Server answers me with a full trace and a dictionnary giving test results:")
+    pprint(server_answer.tests)
+    print("I can see that my solution do not pass all the tests. I propose another one.")
+    server_answer = server.submit_solution(my_token, problem.id, GOOD_SOLUTION)
+    print("This time it's ok:")
+    pprint(server_answer.tests)
+    # print(server_answer.full_trace)
+    assert all(server_answer.tests.values())
+
+    print('\n\nI can also send new unit tests.')
+    server.submit_test(my_token, problem.id, NEW_UNIT_TEST)
+
+
+    print('\n\nI can send again my solution, to prove that i pass the new test.')
+    server_answer = server.submit_solution(my_token, problem.id, GOOD_SOLUTION)
+    print("Server answers me with a full trace and a dictionnary giving test results:")
+    pprint(server_answer.tests)
+    assert all(server_answer.tests.values())
+
+
+    print('\n\n\n\n')
+    print('#' * 80)
+    print('# ROOTER PART II')
+    print('#' * 80)
+    print('Rooter will close the session, because deadline is met.')
+    server.close_problem_session(rooter_token, problem.id)
+
+
+    print('\n\n\n\n')
+    print('#' * 80)
+    print('# PLAYER PART II')
+    print('#' * 80)
+    print("As a player, i can't push new solutions since the sesson is over:")
+    server_answer = server.submit_solution(my_token, problem.id, GOOD_SOLUTION)
+    pprint(server_answer.tests)
+
+
+
+    print('\n\n\n\n')
+    print('#' * 80)
+    print('# ROOTER PART III')
+    print('#' * 80)
+    print('Rooter will now ask for report about students work.')
+    print('Rooter can retrieve students implied in the problem:')
+    players = server.retrieve_players_of(rooter_token, problem.id)
+    print('\t' + '\n\t'.join(players))
+    choosen_player = random.choice(players)
+    print('Rooter looks at the progress of student {} by asking a report to the server:'
+          ''.format(choosen_player))
+
+    print('\n\n')
+    report = server.retrieve_report(choosen_player, problem.id)
+    print(report)
+
+
 DESCRIPTION = """Writing in a module *revcomp.py* a function *revcomp*
 that convert ATGC to TACG and reverse the sequence.
 Insert here more biological background and formal explanations,
@@ -47,52 +156,6 @@ def test_multinuc_5():
     assert revcomp('acgacataa') == 'ttatgtcgt'
 """)
 
-print('#' * 80)
-print('# ROOTER PART')
-print('#' * 80)
-
-
-# Registering is performed by the rooter.
-# It gives to the server the exact definition of the problem,
-#  that any player can retrieve.
-PLAYER_PASSWORD = 'WOLOLO42'
-server = weldon.Server(player_password=PLAYER_PASSWORD)
-rooter_token = server.register_rooter('gérard')
-print("Rooter register itself as a rooter, getting (secret) token {}."
-      "".format(rooter_token))
-problem = server.register_problem(
-    rooter_token,
-    'problem01',
-    DESCRIPTION,
-    public_tests=PUBLIC_TESTS,
-    hidden_tests=HIDDEN_TESTS,
-    # TODO: implement rules (or additionnal options on problem)
-    # rules=['allow upload test if all tests succeed',
-           # 'allow upload solution if player or rooter']  # should be a default
-)
-print("Rooter, using (secret) token, create a new problem on the server.")
-print("Rooter gives to the player the password '{}', to use to register."
-      "".format(PLAYER_PASSWORD))
-
-
-
-
-print('\n\n')
-print('#' * 80)
-print('# PLAYER PART')
-print('#' * 80)
-
-print("First, i need to register on server, as a player."
-      " Rooter gives me the password '{}'".format(PLAYER_PASSWORD))
-my_token = server.register_player('lucas', password=PLAYER_PASSWORD)
-print('My (secret) token is', my_token)
-
-print('\nUsing my token, i can ask for specific problem at server.')
-problem = server.retrieve_public_problem(my_token, 'problem01')
-print('The problem given by the rooter is {}.'.format(problem.title))
-print('Full object given by server:\n', problem, '\n\n', sep='')
-
-
 BAD_SOLUTION = """
 def revcomp(sequence):
     complement = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G',
@@ -108,45 +171,11 @@ def revcomp(sequence):
     except KeyError:
         raise ValueError("Input sequence is not an ATGC one.")
 """
-
-print('I code my solution, and send it to the server using my token.')
-server_answer = server.submit_solution(my_token, problem.id, BAD_SOLUTION)
-print("Server answers me with a full trace and a dictionnary giving test results:")
-pprint(server_answer.tests)
-print("I can see that my solution do not pass all the tests. I propose another one.")
-server_answer = server.submit_solution(my_token, problem.id, GOOD_SOLUTION)
-print("This time it's ok:")
-pprint(server_answer.tests)
-# print(server_answer.full_trace)
-
-
-print('\n\nI can also send new unit tests.')
-server.submit_test(my_token, problem.id, """
+NEW_UNIT_TEST = """
 def test_sent_by_lucas():
     assert revcomp('AAT') == 'ATT'
-""")
+"""
 
 
-print('\n\nI can send again my solution, to prove that i pass the new test.')
-server_answer = server.submit_solution(my_token, problem.id, GOOD_SOLUTION)
-print("Server answers me with a full trace and a dictionnary giving test results:")
-pprint(server_answer.tests)
-
-
-print('\n\n\n\n')
-print('#' * 80)
-print('# ROOTER PART II')
-print('#' * 80)
-print('Rooter will close the session, because deadline is met.')
-server.close_problem_session(rooter_token, problem.id)
-print('Rooter will now ask for report about students work.')
-print('Rooter can retrieve students implied in the problem:')
-players = server.retrieve_players_of(rooter_token, problem.id)
-print('\t' + '\n\t'.join(players))
-choosen_player = random.choice(players)
-print('Rooter looks at the progress of student {} by asking a report to the server:'
-      ''.format(choosen_player))
-
-print('\n\n')
-report = server.retrieve_report(choosen_player, problem.id)
-print(report)
+if __name__ == "__main__":
+    test_story_problem01()
