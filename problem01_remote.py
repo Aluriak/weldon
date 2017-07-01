1ec89f9186dc4a3bfdf26d6507c091d2473cbbe3
Aluriak <lucas.bourneuf@laposte.net>
2017-07-01 14:10:57 +0200

    problem01_remote: same story as problem01 + full use of webclient

--- problem01_remote.py
+++ problem01_remote.py
@@ -1,5 +1,6 @@
 
 
+import random
 from pprint import pprint
 
 from webserver import PORT as TCP_PORT, BUFFER_SIZE
@@ -39,6 +40,26 @@ def test_multinuc_4():
 def test_multinuc_5():
     assert revcomp('acgacataa') == 'ttatgtcgt'
 """)
+BAD_SOLUTION = """
+def revcomp(sequence):
+    complement = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G',
+                  'a': 't', 't': 'a', 'g': 'c', 'c': 'g'}
+    return ''.join(reversed(tuple(complement[letter] for letter in sequence)))
+"""
+GOOD_SOLUTION = """
+def revcomp(sequence):
+    complement = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G',
+                  'a': 't', 't': 'a', 'g': 'c', 'c': 'g'}
+    try:
+        return ''.join(reversed(tuple(complement[letter] for letter in sequence)))
+    except KeyError:
+        raise ValueError("Input sequence is not an ATGC one.")
+"""
+NEW_UNIT_TEST = """
+def test_sent_by_lucas():
+    assert revcomp('AAT') == 'ATT'
+"""
+
 
 print('#' * 80)
 print('# ROOTER PART')
@@ -85,34 +106,73 @@ problem = conn.retrieve_public_problem()
 print('The problem given by the rooter is {}.'.format(problem.title))
 print('Full object given by server:\n', problem, '\n\n', sep='')
 
-
-SOLUTION = """
-def revcomp(sequence):
-    complement = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G',
-                  'a': 't', 't': 'a', 'g': 'c', 'c': 'g'}
-    try:
-        return ''.join(reversed(tuple(complement[letter] for letter in sequence)))
-    except KeyError:
-        raise ValueError("Input sequence is not an ATGC one.")
-"""
+print("\nNote that, as a player, i can't submit new problems.")
+# Also, the Send object do not provide non-accessible API.
+#  so, we have to bypass it to perform the problem registering.
+try:
+    have_failed = False
+    def failed_on(payload):  raise ValueError()
+    conn._send(
+        'register_problem', token=conn.token, title='title',
+        description='description', public_tests='tests',
+        hidden_tests='tests', failed_on=failed_on
+    )
+    assert False, "Call to 'register_problem' as a student did not raise any error."
+except ValueError:
+    pass
 
 print('I code my solution, and send it to the server using my token.')
-server_answer = conn.submit_solution(SOLUTION)
+server_answer = conn.submit_solution(BAD_SOLUTION)
 print("Server answers me with a full trace and a dictionnary giving test results:")
-pprint(server_answer)
+pprint(server_answer.tests)
+print("I can see that my solution do not pass all the tests. I propose another one.")
+server_answer = conn.submit_solution(GOOD_SOLUTION)
+print("This time it's ok:")
 pprint(server_answer.tests)
 # print(server_answer.full_trace)
-
+assert all(test.succeed for test in server_answer.tests)
 
 print('\n\nI can also send new unit tests.')
-conn.submit_test("""
-def test_sent_by_elucator():
-    assert revcomp('AAT') == 'ATT'
-""")
+conn.submit_test(NEW_UNIT_TEST)
 
 
 print('\n\nI can send again my solution, to prove that i pass the new test.')
-
-server_answer = conn.submit_solution(SOLUTION)
+server_answer = conn.submit_solution(GOOD_SOLUTION)
 print("Server answers me with a full trace and a dictionnary giving test results:")
 pprint(server_answer.tests)
+assert all(test.succeed for test in server_answer.tests)
+
+
+print('\n\n\n\n')
+print('#' * 80)
+print('# ROOTER PART II')
+print('#' * 80)
+print('Rooter will close the session, because deadline is met.')
+root_conn.close_problem_session()
+
+
+print('\n\n\n\n')
+print('#' * 80)
+print('# PLAYER PART II')
+print('#' * 80)
+print("As a player, i can't push new solutions since the sesson is over:")
+server_answer = conn.submit_solution(GOOD_SOLUTION)
+pprint(server_answer.tests)
+
+
+
+print('\n\n\n\n')
+print('#' * 80)
+print('# ROOTER PART III')
+print('#' * 80)
+print('Rooter will now ask for report about students work.')
+print('Rooter can retrieve students implied in the problem:')
+players = root_conn.retrieve_players_of()
+print('\t' + '\n\t'.join(players))
+choosen_player = random.choice(players)
+print('Rooter looks at the progress of student {} by asking a report to the server:'
+      ''.format(choosen_player))
+
+print('\n\n')
+report = server.retrieve_report(choosen_player)
+print(report)
