1c3a78d4be953300bbf65530e62e55b7b74784f9
Aluriak <lucas.bourneuf@laposte.net>
2017-07-01 03:33:05 +0200

    webclient: remove usecase + implement Send, a real interface to webserver and Server
    Send is working flexibly with server. It do not have to know the Server
    source code, all information is passing using the server (new) API.

--- webclient.py
+++ webclient.py
@@ -1,25 +1,26 @@
 
-import json
 import socket
-from pprint import pprint
-# from ast import literal_eval
+import inspect
 
 import wjson
+from server import Server
 from webserver import PORT as TCP_PORT, BUFFER_SIZE
 
 
-def send(function:str, *args:str, **kwargs:str):
-    TCP_IP = '127.0.0.1'
+TCP_IP = '127.0.0.1'
 
+
+def send(function:str, *args:str, tcp_port:int=TCP_PORT,
+         buffer_size:int=BUFFER_SIZE, host:str=TCP_IP, **kwargs):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((TCP_IP, TCP_PORT))
-    payload = wjson.as_json((function, tuple(args), kwargs)).encode()
+    s.connect((host, tcp_port))
+    payload = wjson.as_json((function, tuple(args), dict(kwargs))).encode()
     s.send(payload)
 
     buffer = True
     received = ''
     while buffer:
-        buffer = s.recv(BUFFER_SIZE).decode()
+        buffer = s.recv(buffer_size).decode()
         received += buffer
     data = wjson.from_json(received)
     s.close()
@@ -31,113 +32,73 @@ def send(function:str, *args:str, **kwargs:str):
         return data['payload']
 
 
-DESCRIPTION = """Writing in a module *revcomp.py* a function *revcomp*
-that convert ATGC to TACG and reverse the sequence.
-Insert here more biological background and formal explanations,
-and also expected behavior on error.
-"""
+class Send:
+    """Wrapper around send function, allowing user to specify only once
+    some parameters (notabily token and problem).
 
-PUBLIC_TESTS = ("""
-def test_onenuc():
-    assert revcomp('A') == 'T'
-    assert revcomp('T') == 'A'
-    assert revcomp('C') == 'G'
-    assert revcomp('G') == 'C'
-""","""
-def test_multinuc_1():
-    assert revcomp('ATCG') == 'CGAT'
-""","""
-def test_multinuc_2():
-    assert revcomp('AAAACAAAA') == 'TTTTGTTTT'
-""","""
-def test_error1():
-    with pytest.raises(ValueError):
-        revcomp('ABCD')
-""")
-HIDDEN_TESTS = ("""
-def test_multinuc_3():
-    assert revcomp('AATACATAA') == 'TTATGTATT'
-""","""
-def test_multinuc_4():
-    assert revcomp('ACGACATAA') == 'TTATGTCGT'
-""","""
-def test_multinuc_5():
-    assert revcomp('acgacataa') == 'ttatgtcgt'
-""")
+    This object adapt itself to available Server API by asking the server
+    directly about available API, so only the api access and the registration
+    needs to be common.
 
-print('#' * 80)
-print('# ROOTER PART')
-print('#' * 80)
+    Consequently, this object do not need the Server source code,
+    and also natively manage rooters access to their specific API.
+
+    """
+
+    def __init__(self, registration_password:str, name:str, problem=None,
+                 root:bool=False, tcp_port:int=TCP_PORT,
+                 buffer_size:int=BUFFER_SIZE, host:str=TCP_IP):
+        self.token = None
+        self.problem_id = problem
+        self.tcp_port = int(tcp_port)
+        self.buffer_size = int(buffer_size)
+        self.host = str(host)
+        self.root = bool(root)
+        self.name = str(name)
+        self.registration_password = str(registration_password)
+        self.known_params = {'token', 'problem', 'problem_id'}
+        self.register()
+        self.implement_api()
 
 
-# Registering is performed by the rooter.
-# It gives to the server the exact definition of the problem,
-#  that any player can retrieve.
-rooter_token = send('register_rooter', 'lucas', 'SHUBISHI')
-print("Rooter register itself as a rooter, getting (secret) token '{}'."
-      "".format(rooter_token))
-
-print("Rooter, using (secret) token, create a new problem on the server.")
-problem = send(
-    'register_problem',
-    rooter_token,
-    'problem01',
-    DESCRIPTION,
-    public_tests=PUBLIC_TESTS,
-    hidden_tests=HIDDEN_TESTS,
-)
-print('Problem registered as', problem.title)
-
-PLAYER_PASSWORD = 'WOLOLO42'
-print("Rooter gives to the player the password '{}', to use to register."
-      "".format(PLAYER_PASSWORD))
+    def register(self):
+        """Perform the registration on the server"""
+        func = 'register_rooter' if self.root else 'register_player'
+        self.token = self._send(func, name=self.name, password=self.registration_password)
 
 
+    def implement_api(self):
+        """Will ask the server about available API.
+        Will dynamically create the methods for self with the parameters
+        that are not deductible from already known information (notabily token).
+
+        This method uses black magic.
+        See https://stackoverflow.com/a/2982/3077939 for details.
+
+        """
+        self.server_api = self._send('get_api', token=self.token)
+        # generate all functions according to received api
+        for name, params in self.server_api.items():
+            if any(not param.isidentifier() for param in params):
+                raise ValueError("Server sent API method parameter that is not "
+                                 "a valid identifier: " + ', '.join(params))
+            method_name = name
+            method_params = tuple(param for param in params if param not in self.known_params)
+            known_params = tuple(param for param in params if param in self.known_params)
+            # generate the method definition
+            method_def = "def {}(self, {}):\n return self._send('{}', {}{}{})".format(
+                method_name, ', '.join(method_params),
+                method_name,
+                ', '.join('{p}=self.{p}'.format(p=param) for param in known_params),
+                ', ' if known_params else '',
+                ', '.join('{p}={p}'.format(p=param) for param in method_params),
+            )
+            # run the code, then __get__ the bounded-to-self version
+            #  of the function, and put it as an attribute of self.
+            exec(method_def)
+            setattr(self, method_name, locals()[method_name].__get__(self))
 
 
-print('\n\n')
-print('#' * 80)
-print('# PLAYER PART')
-print('#' * 80)
-
-print("First, i need to register on server, as a player."
-      " Rooter gives me the password '{}'".format(PLAYER_PASSWORD))
-my_token = send('register_player', 'elucator', password=PLAYER_PASSWORD)
-print('My (secret) token is', my_token)
-
-print('\nUsing my token, i can ask for specific problem at server.')
-problem = send('retrieve_public_problem', my_token, 'problem01')
-print('The problem given by the rooter is {}.'.format(problem.title))
-print('Full object given by server:\n', problem, '\n\n', sep='')
-
-
-SOLUTION = """
-def revcomp(sequence):
-    complement = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G',
-                  'a': 't', 't': 'a', 'g': 'c', 'c': 'g'}
-    try:
-        return ''.join(reversed(tuple(complement[letter] for letter in sequence)))
-    except KeyError:
-        raise ValueError("Input sequence is not an ATGC one.")
-"""
-
-print('I code my solution, and send it to the server using my token.')
-server_answer = send('submit_solution', my_token, problem.id, SOLUTION)
-print("Server answers me with a full trace and a dictionnary giving test results:")
-pprint(server_answer)
-pprint(server_answer.tests)
-# print(server_answer.full_trace)
-
-
-print('\n\nI can also send new unit tests.')
-send('submit_test', my_token, problem.id, """
-def test_sent_by_lucas():
-    assert revcomp('AAT') == 'ATT'
-""")
-
-
-print('\n\nI can send again my solution, to prove that i pass the new test.')
-
-server_answer = send('submit_solution', my_token, problem.id, SOLUTION)
-print("Server answers me with a full trace and a dictionnary giving test results:")
-pprint(server_answer.tests)
+    def _send(self, function, **kwargs):
+        """Send request to the server"""
+        return send(function, **kwargs)
