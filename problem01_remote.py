088b3765cee5ab39bc1cd4b69e6e1108de31e5d4
Aluriak <lucas.bourneuf@laposte.net>
2017-07-01 05:55:18 +0200

    problem01_remote: it's problem01, but using the webclient

---
+++ problem01_remote.py
@@ -0,0 +1,118 @@
+
+
+from pprint import pprint
+
+from webserver import PORT as TCP_PORT, BUFFER_SIZE
+from webclient import Send
+
+
+DESCRIPTION = """Writing in a module *revcomp.py* a function *revcomp*
+that convert ATGC to TACG and reverse the sequence.
+Insert here more biological background and formal explanations,
+and also expected behavior on error.
+"""
+
+PUBLIC_TESTS = ("""
+def test_onenuc():
+    assert revcomp('A') == 'T'
+    assert revcomp('T') == 'A'
+    assert revcomp('C') == 'G'
+    assert revcomp('G') == 'C'
+""","""
+def test_multinuc_1():
+    assert revcomp('ATCG') == 'CGAT'
+""","""
+def test_multinuc_2():
+    assert revcomp('AAAACAAAA') == 'TTTTGTTTT'
+""","""
+def test_error1():
+    with pytest.raises(ValueError):
+        revcomp('ABCD')
+""")
+HIDDEN_TESTS = ("""
+def test_multinuc_3():
+    assert revcomp('AATACATAA') == 'TTATGTATT'
+""","""
+def test_multinuc_4():
+    assert revcomp('ACGACATAA') == 'TTATGTCGT'
+""","""
+def test_multinuc_5():
+    assert revcomp('acgacataa') == 'ttatgtcgt'
+""")
+
+print('#' * 80)
+print('# ROOTER PART')
+print('#' * 80)
+
+
+# Registering is performed by the rooter.
+# It gives to the server the exact definition of the problem,
+#  that any player can retrieve.
+root_conn = Send(name='lucas', registration_password='SHUBISHI', root=True)
+assert root_conn.get_api() == root_conn.server_api  # the first request the server
+
+print("Rooter register itself as a rooter, getting (secret) token '{}'."
+      "".format(root_conn.token))
+
+print("Rooter, using (secret) token, create a new problem on the server.")
+problem = root_conn.register_problem(
+    'problem01',
+    DESCRIPTION,
+    public_tests=PUBLIC_TESTS,
+    hidden_tests=HIDDEN_TESTS,
+)
+print('Problem registered as', problem.title)
+
+PLAYER_PASSWORD = 'WOLOLO42'
+print("Rooter gives to the player the password '{}', to use to register."
+      "".format(PLAYER_PASSWORD))
+
+
+
+print('\n\n')
+print('#' * 80)
+print('# PLAYER PART')
+print('#' * 80)
+
+print("First, i need to register on server, as a player."
+      " Rooter gives me the password '{}'".format(PLAYER_PASSWORD))
+conn = Send(name='elucator', registration_password=PLAYER_PASSWORD)
+print('My (secret) token is', conn.token)
+
+print('\nUsing my token, i can ask for specific problem at server.')
+conn.problem_id = 'problem01'  # could have been given to Send object constructor
+problem = conn.retrieve_public_problem()
+print('The problem given by the rooter is {}.'.format(problem.title))
+print('Full object given by server:\n', problem, '\n\n', sep='')
+
+
+SOLUTION = """
+def revcomp(sequence):
+    complement = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G',
+                  'a': 't', 't': 'a', 'g': 'c', 'c': 'g'}
+    try:
+        return ''.join(reversed(tuple(complement[letter] for letter in sequence)))
+    except KeyError:
+        raise ValueError("Input sequence is not an ATGC one.")
+"""
+
+print('I code my solution, and send it to the server using my token.')
+server_answer = conn.submit_solution(SOLUTION)
+print("Server answers me with a full trace and a dictionnary giving test results:")
+pprint(server_answer)
+pprint(server_answer.tests)
+# print(server_answer.full_trace)
+
+
+print('\n\nI can also send new unit tests.')
+conn.submit_test("""
+def test_sent_by_elucator():
+    assert revcomp('AAT') == 'ATT'
+""")
+
+
+print('\n\nI can send again my solution, to prove that i pass the new test.')
+
+server_answer = conn.submit_solution(SOLUTION)
+print("Server answers me with a full trace and a dictionnary giving test results:")
+pprint(server_answer.tests)
