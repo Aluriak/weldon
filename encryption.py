860b6927e370ef7d1823ead16f5c6862e537f851
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2017-08-20 11:09:09 +0200

    encryption: uses PCKS1_OAEP cipher
    needed for really secure encryption

--- encryption.py
+++ encryption.py
@@ -6,6 +6,7 @@ If the module is not available, will fall back on no-encryption.
 
 try:
     import Crypto
+    from Crypto.Cipher import PKCS1_OAEP
     from Crypto.PublicKey import RSA
     from Crypto import Random
     from Crypto.Hash import MD5
@@ -21,23 +22,24 @@ random_gen = Random.new().read if Crypto else None
 
 def generate_key_pair():
     """Return a key pair"""
-    return RSA.generate(KEY_LENGTH, random_gen) if Crypto else None
+    return RSA.generate(KEY_LENGTH, e=65537) if Crypto else None
 
 def encrypt(data:bytes, public_key) -> bytes:
     """Return given data, encrypted using public key"""
     if Crypto and public_key is not None:
-        help(public_key.encrypt)
-        return public_key.encrypt(data, int.from_bytes(random_gen(4), 'big'))
+        cipher = PKCS1_OAEP.new(RSA.importKey(public_key.exportKey("DER")))
+        return cipher.encrypt(data)
     return data
 
 def decrypt(data:bytes, keypair) -> bytes:
     """Return given data, decrypted using private key"""
     if Crypto and keypair is not None:
-        return keypair.decrypt(data)
+        key = RSA.importKey(keypair.exportKey("DER"))
+        cipher = PKCS1_OAEP.new(key)
+        return cipher.decrypt(data)
     return data
 
 def public_key_from_key_pair(keypair):
     if Crypto and keypair is not None:
         return keypair.publickey()
     return None
-
