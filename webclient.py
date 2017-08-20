ff3607f7b79c5ef725e0df72610f16f8f4ec391d
Lucas Bourneuf <lucas.bourneuf@laposte.net>
2017-08-20 14:31:10 +0200

    webclient: now handle encryption if a keypair is given by client code
    note that, in order to handle encryption, data exchange protocol has
    changed a little. Now, both client and server send a dictionary with two
    keys: encryption_key (null when no encryption) and payload, containing
    the (encrypted) data.
    Server also provides a status value.
    
    See README for more information.

--- webclient.py
+++ webclient.py
@@ -1,21 +1,49 @@
 
 import socket
+import base64
 import inspect
 
 import wjson
 from server import Server
 from commons import ServerError
 from webserver import PORT as TCP_PORT, BUFFER_SIZE
+from hybrid_encryption import HybridEncryption
 
 
 TCP_IP = '127.0.0.1'
 
 
-def send(function:str, *args:str, port:int=TCP_PORT,
-         buffer_size:int=BUFFER_SIZE, host:str=TCP_IP, **kwargs):
+def create_payload(function:str, *args:str, keypair=None, server_pubkey=None, **kwargs) -> bytes:
+    """Create and return the payload.
+
+    Will encrypt it if keypair and server public key are given.
+
+    """
+    print('COMMAND:', function)
+    payload = wjson.as_json((function, tuple(args), dict(kwargs)))
+    print('RAW PAYLOAD:', payload)
+    key = None
+    if keypair and server_pubkey:
+        payload, key = keypair.encrypt(payload, server_pubkey)
+        payload = base64.b64encode(payload).decode()  # convert binary into str
+        key = base64.b64encode(key).decode()
+    assert isinstance(key, str) or key is None
+    assert isinstance(payload, str)
+    data = wjson.as_json({
+        'encryption_key': key,
+        'payload': payload,
+    })
+    print('READY PAYLOAD:', data)
+    print('ENCODED PAYLOAD:', data.encode())
+    return data.encode()
+
+
+def send(payload:bytes, port:int=TCP_PORT, buffer_size:int=BUFFER_SIZE,
+         host:str=TCP_IP) -> bytes:
+    """Send and retrieve the answer through TCP socket"""
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((host, port))
-    payload = wjson.as_json((function, tuple(args), dict(kwargs))).encode()
+    print('PAYLOAD TO SEND:', payload)
     s.send(payload)
 
     buffer = True
@@ -23,13 +51,30 @@ def send(function:str, *args:str, port:int=TCP_PORT,
     while buffer:
         buffer = s.recv(buffer_size).decode()
         received += buffer
+    print('RECEIVED:', received)
     data = wjson.from_json(received)
     s.close()
+    return data
 
+
+def extract_payload(data:dict, keypair=None) -> str:
+    """Return the (decrypted) payload found in data, or raise a ServerError
+    when failed status"""
+    assert data['status'] in {'failed', 'succeed'}, 'status is not failed nor succeed'
     if data['status'] == 'failed':
         raise ServerError(data['payload'])
-    else:
-        return data['payload']
+    elif data['encryption_key']:  # success & encrypted
+        payload = base64.b64decode(data['payload'])
+        key = base64.b64decode(data['encryption_key'])
+        if not keypair:
+            raise ValueError("Payload is encrypted, but keypair is not provided")
+        print('SVFJGL RECEIVED KEY:', data['encryption_key'])
+        print('SVFJGL PUBKEY:', key)
+        ret = keypair.decrypt(payload, key)
+    else:  # succeed but not encrypted
+        ret = data['payload']
+    print('RECEIVED PAYLOAD:', ret)
+    return ret
 
 
 class Send:
@@ -47,7 +92,8 @@ class Send:
 
     def __init__(self, registration_password:str, name:str, problem=None,
                  root:bool=False, port:int=TCP_PORT,
-                 buffer_size:int=BUFFER_SIZE, host:str=TCP_IP):
+                 buffer_size:int=BUFFER_SIZE, host:str=TCP_IP,
+                 keypair:HybridEncryption=None):
         self.token = None
         self.problem_id = problem
         self.port = int(port)
@@ -55,16 +101,29 @@ class Send:
         self.host = str(host)
         self.root = bool(root)
         self.name = str(name)
+        self.keypair = keypair
         self.registration_password = str(registration_password)
         self.known_params = {'token', 'problem', 'problem_id'}
+        self.get_server_pubkey()
         self.register()
         self.implement_api()
+
+    def get_server_pubkey(self):
+        """Contact the server in order to get its public key"""
+        self.server_pubkey = None
+        self.server_pubkey = self._send(command='get_public_key')
+        print('SERVER PUBLIC KEY:', self.server_pubkey)
 
 
     def register(self):
         """Perform the registration on the server"""
-        func = 'register_rooter' if self.root else 'register_player'
-        self.token = self._send(func, name=self.name, password=self.registration_password)
+        register = 'register_rooter' if self.root else 'register_player'
+        self.token = self._send(
+            command=register,
+            name=self.name,
+            password=self.registration_password,
+            public_key=self.keypair.publickey_as_string if self.keypair else None,
+        )
 
 
     def implement_api(self):
@@ -99,6 +158,12 @@ class Send:
             setattr(self, method_name, locals()[method_name].__get__(self))
 
 
-    def _send(self, function, **kwargs):
+    def _send(self, command, **kwargs):
         """Send request to the server"""
-        return send(function, **kwargs)
+        payload = create_payload(command, **kwargs,
+                                 keypair=self.keypair,
+                                 server_pubkey=self.server_pubkey)
+        return wjson.from_json(extract_payload(send(
+            payload, port=self.port,
+            buffer_size=self.buffer_size, host=self.host
+        ), keypair=self.keypair))
